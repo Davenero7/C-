91a89d7565f361ff629236de8f0d77e66bea4da3
davenero7 <dave7.spark@gmail.com>
2019-06-18 01:50:53 -0500

    test

---
+++ AdivinarNumero/Adivina.hpp
@@ -0,0 +1,14 @@
+//
+//  Adivina.hpp
+//  AdivinarNumero
+//
+//  Created by David Hernandez Villeda on 04/05/18.
+//  Copyright © 2018 David Hernandez Villeda. All rights reserved.
+//
+
+#ifndef Adivina_hpp
+#define Adivina_hpp
+
+#include <stdio.h>
+
+#endif /* Adivina_hpp */
